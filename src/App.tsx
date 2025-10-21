0dbaa509ce4557143f77b6f4eb9c2bd0cd2beb77
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-10-20 20:27:09 -0700

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,41 @@
-import MainLayout from './layout/MainLayout';
-import LoginDialog from './components/auth/dialogs/LoginDialog';
-import RegisterDialog from './components/auth/dialogs/RegisterDialog';
-import ForgotPaswordDialog from './components/auth/dialogs/ForgotPaswordDialog';
-import ResetPaswordDialog from './components/auth/dialogs/ResetPasswordDialog';
-import Home from './pages/Home';
-import ProtectedRoute from './routers/ProtectedRoute';
-import { AuthProvider } from './context/AuthProvider';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { lazy } from 'react';
-import './App.css';
+import MainLayout from './layout/MainLayout'
+import LoginDialog from './components/auth/dialogs/LoginDialog'
+import RegisterDialog from './components/auth/dialogs/RegisterDialog'
+// import ForgotPaswordDialog from './components/auth/dialogs/ForgotPaswordDialog';
+import ResetPaswordDialog from './components/auth/dialogs/ResetPasswordDialog'
+import Home from './pages/Home'
+import ProtectedRoute from './routers/ProtectedRoute'
+import { AuthProvider } from './context/AuthProvider'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { lazy } from 'react'
+import './App.css'
 
-const Dashboard = lazy(() => import('./pages/Dashboard'));
-const Tasks = lazy(() => import('./pages/Tasks'));
-const TaskDialog = lazy(() => import('./components/tasks/dialogs/TaskDialog'));
+const Dashboard = lazy(() => import('./pages/Dashboard'))
+const Tasks = lazy(() => import('./pages/Tasks'))
+const TaskDialog = lazy(() => import('./components/tasks/dialogs/TaskDialog'))
 
-function App() {
+function App () {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainLayout />}>
-             <Route index element={<Home />} />
-            <Route path="register" element={<RegisterDialog />} />
-            <Route path="login" element={<LoginDialog />} />
-            <Route path="forgotPassword" element={<ForgotPaswordDialog />} />
-            <Route path="resetPassword" element={<ResetPaswordDialog />} />
+          <Route path='/' element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path='register' element={<RegisterDialog />} />
+            <Route path='login' element={<LoginDialog />} />
+            {/* <Route path="forgotPassword" element={<ForgotPaswordDialog />} /> */}
+            <Route path='resetPassword' element={<ResetPaswordDialog />} />
             <Route element={<ProtectedRoute />}>
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="tasks" element={<Tasks />} />
-              <Route path="tasks/:id" element={<TaskDialog />} />
+              <Route path='dashboard' element={<Dashboard />} />
+              <Route path='tasks' element={<Tasks />} />
+              <Route path='tasks/:id' element={<TaskDialog />} />
             </Route>
-            <Route path="*" element={<p>Page Not Found</p>} />
+            <Route path='*' element={<p>Page Not Found</p>} />
           </Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
-  );
+  )
 }
 
-export default App;
+export default App
